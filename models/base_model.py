17a9713506aedc5d25c834102471b857eee83302
niialabi <taufiq.alabi@gmail.com>
2023-10-11 19:49:59 +0000

    task 3 midway point

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+"""BaseModel that defines all common attributes/methods for other classes:"""
+
+from uuid import uuid4
+from datetime import datetime
+import time
+
+
+class BaseModel():
+    """
+    Base Class containing all attributes/methods for other classes
+    """
+    def __init__(self):
+        """
+        class instatiation
+
+        Args:
+            id: uniq_id of obj
+
+        """
+        self.id = str(uuid4())
+        self.created_at = str(datetime.now().isoformat())
+        self.updated_at = str(datetime.now().isoformat())
+
+    def __str__(self):
+        """
+        string rep of BaseModel Object
+        """
+        return("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))

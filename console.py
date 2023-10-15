27a39cf3f1adc1cd71429124e62a09041e2bf8c7
niialabi <taufiq.alabi@gmail.com>
2023-10-15 13:08:26 +0000

    pycode fix -- console

--- console.py
+++ console.py
@@ -9,15 +9,13 @@ import cmd
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb)"
 
-    
-
     def do_EOF(self, line):
         """
         End of file signal ends program
         """
         return True
-    
-    def do_quit(self,quit):
+
+    def do_quit(self, line):
         """
         "quit" ends program
         """
@@ -32,4 +30,3 @@ class HBNBCommand(cmd.Cmd):
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
-
